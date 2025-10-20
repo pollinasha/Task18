1b7c66536b46bb10c2fdead0084094f549161d9f
pollinasha <151538655+pollinasha@users.noreply.github.com>
2025-10-20 09:57:52 +0300

    Delete Task18 directory

--- Task18/src/Task3/InputException.java
+++
@@ -1,8 +0,0 @@
-package Task3;
-
-public class InputException extends Exception {
-    public InputException(final String message){
-        super(message);
-
-    }
-}
--- Task18/src/Task3/LimitException.java
+++
@@ -1,9 +0,0 @@
-package Task3;
-
-public class LimitException extends RuntimeException {
-    public LimitException(final String message, final int attempts){
-        super(message);
-
-
-    }
-}
